2a3d0608b5775abaea25bfac369b9679a48e758d
Bruno Gaudio <brunogmattos22@gmail.com>
2023-05-25 15:50:46 -0300

    Associando o Projeto com o Repositório Local e Suas Dependências Necessárias no Pom.xml

---
+++ src/main/java/br/com/azematti/AzemattiPerformanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.azematti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzemattiPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzemattiPerformanceApplication.class, args);
+	}
+
+}
